d7a73179c79e134921cd7b0052c6b034ad91dfa0
Michael Napoli <mikednapoli@gmail.com>
2023-03-08 12:22:28 -0500

    comments added

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import machine
 # user-made libraries
 from Motor import *
 
-#--------------------------------------#
+# global variable section
 LED = machine.Pin(2, machine.Pin.OUT);
 LED.on();
 
@@ -18,7 +18,8 @@ motPins = ((25, 26, 27), (5, 18, 17));
 m1 = Motor(motPins[0]);
 m2 = Motor(motPins[1]);
 
-for i in range(3):
+# blink twice if successful
+for i in range(2):
     LED.on();
     time.sleep(250/1000);
     LED.off();
